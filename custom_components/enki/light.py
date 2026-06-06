0fad271bdbbc979af3d559b0f7f9c023b6fb3cc4
StephaneBranly <stephanebranly.pro@gmail.com>
2026-06-06 16:22:23 +0200

    [+] Support for RGB lights

--- custom_components/enki/light.py
+++ custom_components/enki/light.py
@@ -7,11 +7,14 @@ from homeassistant.components.light import ColorMode, LightEntity
 from homeassistant.components.light.const import DEFAULT_MIN_KELVIN, DEFAULT_MAX_KELVIN 
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.device_registry import DeviceInfo
+from homeassistant.util.color import value_to_brightness, brightness_to_value
 
+import math
 from . import EnkiConfigEntry
 from .base import EnkiBaseEntity
 from .coordinator import EnkiCoordinator
-from .const import LOGGER
+from .const import LOGGER, DOMAIN
 
 async def async_setup_entry(
     hass: HomeAssistant,
@@ -20,12 +23,14 @@ async def async_setup_entry(
 ):
     """Set up the Binary Sensors."""
     coordinator: EnkiCoordinator = config_entry.runtime_data.coordinator
-
+    LOGGER.debug(f"coordinator {coordinator.data}")
     lights = [
         entity
         for device in coordinator.data
         for entity in _build_light_entities(coordinator, device)
     ]
+    LOGGER.debug(f"lights > {lights}")
+
     async_add_entities(lights)
 
 class EnkiLight(EnkiBaseEntity, LightEntity):
@@ -34,7 +39,10 @@ class EnkiLight(EnkiBaseEntity, LightEntity):
     _attr_color_mode = None
     _attr_min_color_temp_kelvin = None
     _attr_max_color_temp_kelvin = None
+
     BRIGHTNESS_SCALE = (1,255)
+    SATURATION_SCALE = (1,100)
+    HUE_SCALE = (1,100)
 
     def __init__(
         self,
@@ -60,9 +68,15 @@ class EnkiLight(EnkiBaseEntity, LightEntity):
             LOGGER.debug("brightness max : " + str(max_value))
             self.BRIGHTNESS_SCALE = (min_value, max_value)
 
+        
+        if "change_hue" in capabilities or "change_saturation" in capabilities:
+            self._attr_supported_color_modes.add(ColorMode.HS)
+            self._attr_color_mode = ColorMode.HS
+
         if "change_color_temperature" in capabilities:
             self._attr_supported_color_modes.add(ColorMode.COLOR_TEMP)
-            self._attr_color_mode = ColorMode.COLOR_TEMP
+            if self._attr_color_mode is None:
+                self._attr_color_mode = ColorMode.COLOR_TEMP
             if "possibleValues" in device and "change_color_temperature" in device["possibleValues"]:
                 values = device["possibleValues"]["change_color_temperature"]["values"]
                 min_value = int(values[0][1:-1])
@@ -162,7 +176,7 @@ class EnkiLight(EnkiBaseEntity, LightEntity):
         if self._endpoint_id is not None:
             self.coordinator.update_endpoint_power(self.node_id, self._endpoint_id, power)
         else:
-            self.coordinator.update_data(self.node_id, "lastReportedValue", "power", power)
+            self.coordinator.update_data(self.node_id, {"lastReportedValue": {"power": power}})
 
     async def _turn_on_with_mixed_endpoint_workaround(self) -> None:
         """Send OFF->ON when needed to force a fresh ON transition for all lights."""
@@ -170,19 +184,17 @@ class EnkiLight(EnkiBaseEntity, LightEntity):
             await self.coordinator.api.change_light_state(
                 self._device["homeId"],
                 self._device["nodeId"],
-                "power",
-                "OFF",
+                {"power": "OFF"},
             )
-            self.coordinator.update_data(self.node_id, "lastReportedValue", "power", "OFF")
+            self.coordinator.update_data(self.node_id, {"lastReportedValue": {"power": "OFF"}})
             self._optimistic_update_light_endpoint_power("OFF")
 
         await self.coordinator.api.change_light_state(
             self._device["homeId"],
             self._device["nodeId"],
-            "power",
-            "ON",
+            {"power": "ON"},
         )
-        self.coordinator.update_data(self.node_id, "lastReportedValue", "power", "ON")
+        self.coordinator.update_data(self.node_id, {"lastReportedValue": {"power": "ON"}})
         self._optimistic_update_light_endpoint_power("ON")
 
     async def async_turn_on(self, **kwargs: Any) -> None:
@@ -197,16 +209,27 @@ class EnkiLight(EnkiBaseEntity, LightEntity):
 
         if "brightness" in kwargs:
             ha_value = kwargs["brightness"]
-            value = round(ha_value / (255/self.BRIGHTNESS_SCALE[1]), 2)
+            value = math.ceil(brightness_to_value(self.BRIGHTNESS_SCALE, ha_value))
             LOGGER.debug(f"setting brightness value to {ha_value} => {value}")
-            await self.coordinator.api.change_light_state(self._device["homeId"], self._device["nodeId"], "brightness", value)
-            self.coordinator.update_data(self.node_id, "lastReportedValue", "brightness", value)
+            await self.coordinator.api.change_light_state(self._device["homeId"], self._device["nodeId"], {"brightness": value})
+            self.coordinator.update_data(self.node_id, {"lastReportedValue": {"brightness": value}})
         elif "color_temp_kelvin" in kwargs:
+            new_color_mode = 'ct'
             ha_value = kwargs["color_temp_kelvin"]
             value = self.closest_temp_value(ha_value)
             LOGGER.debug("setting color temp to closest value : " + str(ha_value) + " => " + str(value))
-            await self.coordinator.api.change_light_state(self._device["homeId"], self._device["nodeId"], "colorTemperature", "T" + str(value) + "K")
-            self.coordinator.update_data(self.node_id, "lastReportedValue", "colorTemperature", "T" + str(value) + "K")
+            await self.coordinator.api.change_light_state(self._device["homeId"], self._device["nodeId"], {"colorMode": new_color_mode, "colorTemperature": "T" + str(value) + "K" })
+            self.coordinator.update_data(self.node_id, {"lastReportedValue": {"colorTemperature": "T" + str(value) + "K", "colorMode": new_color_mode}})
+        elif "hs_color" in kwargs:
+            new_color_mode = 'hs'
+            LOGGER.debug(f"setting hue to {kwargs['hs_color'][0]} and saturation to {kwargs['hs_color'][1]}")
+            ha_hue, ha_saturation = kwargs["hs_color"]
+            hue_value = round(ha_hue / 360, 2)
+            saturation_value = round(ha_saturation /100, 2)
+            LOGGER.debug(f"setting hue to {ha_hue} => {hue_value}")
+            LOGGER.debug(f"setting saturation to {ha_saturation} => {saturation_value}")
+            await self.coordinator.api.change_light_state(self._device["homeId"], self._device["nodeId"], {"colorMode": new_color_mode, "hue": hue_value, "saturation": saturation_value})
+            self.coordinator.update_data(self.node_id, {"lastReportedValue": {"hue", hue_value, "saturation", saturation_value,  "colorMode", new_color_mode}})
 
     async def async_turn_off(self, **kwargs: Any) -> None:
         """Turn the entity off."""
@@ -214,8 +237,8 @@ class EnkiLight(EnkiBaseEntity, LightEntity):
         # Until the API supports per-endpoint control without side-effects, use change_light_state
         # for all light entities regardless of whether they have an endpoint_id. This will turn off
         # all the lights but at least will not turn off the fan or other non-light endpoints.
-        await self.coordinator.api.change_light_state(self._device["homeId"], self._device["nodeId"], "power", "OFF")
-        self.coordinator.update_data(self.node_id, "lastReportedValue", "power", "OFF")
+        await self.coordinator.api.change_light_state(self._device["homeId"], self._device["nodeId"], {"power": "OFF"})
+        self.coordinator.update_data(self.node_id, {"lastReportedValue": {"power": "OFF"}})
         self._optimistic_update_light_endpoint_power("OFF")
 
     @property
@@ -224,7 +247,7 @@ class EnkiLight(EnkiBaseEntity, LightEntity):
         last_reported_values = self.coordinator.get_device_parameter(self.node_id, "lastReportedValue")
         if "brightness" not in last_reported_values:
             return None
-        return last_reported_values["brightness"]*(255/self.BRIGHTNESS_SCALE[1])
+        return value_to_brightness(self.BRIGHTNESS_SCALE, last_reported_values["brightness"])
     
     @property
     def color_temp_kelvin(self) -> int | None:
@@ -234,12 +257,23 @@ class EnkiLight(EnkiBaseEntity, LightEntity):
             return None
         return int(last_reported_values["colorTemperature"][1:-1])
 
+    @property
+    def hs_color(self) -> tuple[float, float] | None:
+        """Return the color in HS format."""
+        last_reported_values = self.coordinator.get_device_parameter(self.node_id, "lastReportedValue")
+        if "hue" not in last_reported_values or "saturation" not in last_reported_values:
+            return None
+        hue = last_reported_values["hue"] *(360/self.HUE_SCALE[1])
+        saturation = last_reported_values["saturation"] *(100/self.SATURATION_SCALE[1])
+        return (hue, saturation)
+
 
 def _build_light_entities(coordinator: EnkiCoordinator, device: dict[str, Any]) -> list[LightEntity]:
     """Create light entities from power capability and BFF endpoint metadata."""
     if not _has_switch_electrical_power(device):
-
         return []
+
+    if device['type'] != 'lights': return []
 
     endpoint_ids = _main_change_capability_endpoint_ids(device)
     if endpoint_ids:
